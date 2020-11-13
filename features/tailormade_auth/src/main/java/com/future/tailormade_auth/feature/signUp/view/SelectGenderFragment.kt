f67e652bb454cb6cf6efe14bd5d5e14a1ff75652
syntialai <53588149+syntialai@users.noreply.github.com>
2020-11-13 10:40:29 +0700

    SETUP-AUTH: Add listener for checked radio button

--- features/tailormade_auth/src/main/java/com/future/tailormade_auth/feature/signUp/view/SelectGenderFragment.kt
+++ features/tailormade_auth/src/main/java/com/future/tailormade_auth/feature/signUp/view/SelectGenderFragment.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import androidx.core.view.forEach
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.future.tailormade.base.view.BaseFragment
+import com.future.tailormade_auth.R
 import com.future.tailormade_auth.databinding.FragmentSelectGenderBinding
 import com.future.tailormade_auth.feature.signUp.viewmodel.SignUpViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,6 +34,17 @@ import kotlinx.coroutines.InternalCoroutinesApi
     with(binding) {
       buttonSubmitGender.setOnClickListener {
         submitGender(radioGroupSelectGender.checkedRadioButtonId)
+      }
+
+      radioGroupSelectGender.setOnCheckedChangeListener { group, checkedId ->
+        val selectedRadioButton = group.findViewById<RadioButton>(checkedId)
+        selectedRadioButton.setTextColor(resources.getColor(R.color.color_primary_800))
+
+        group.forEach {
+          if (it != selectedRadioButton) {
+            (it as RadioButton).setTextColor(resources.getColor(R.color.color_black_87))
+          }
+        }
       }
     }
 
--- features/tailormade_auth/src/main/java/com/future/tailormade_auth/feature/signUp/view/SelectRoleFragment.kt
+++ features/tailormade_auth/src/main/java/com/future/tailormade_auth/feature/signUp/view/SelectRoleFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import androidx.core.view.forEach
 import androidx.fragment.app.viewModels
 import com.future.tailormade.base.view.BaseFragment
+import com.future.tailormade_auth.R
 import com.future.tailormade_auth.databinding.FragmentSelectRoleBinding
 import com.future.tailormade_auth.feature.signUp.viewmodel.SignUpViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,6 +34,17 @@ class SelectRoleFragment : BaseFragment() {
     with(binding) {
       buttonSubmitRole.setOnClickListener {
         submitRole(radioGroupSelectRole.checkedRadioButtonId)
+      }
+
+      radioGroupSelectRole.setOnCheckedChangeListener { group, checkedId ->
+        val selectedRadioButton = group.findViewById<RadioButton>(checkedId)
+        selectedRadioButton.setTextColor(resources.getColor(R.color.color_primary_800))
+
+        group.forEach {
+          if (it != selectedRadioButton) {
+            (it as RadioButton).setTextColor(resources.getColor(R.color.color_black_87))
+          }
+        }
       }
     }
 
