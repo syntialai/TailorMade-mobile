bf83e312567043708cf772aec0c13bd9cd80b006
syntialai <53588149+syntialai@users.noreply.github.com>
2020-11-16 12:43:39 +0700

    SETUP-EDIT-PROFILE: Resolve conflicts

---
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/editProfile/EditProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.future.tailormade_profile.feature.editProfile
+
+import androidx.lifecycle.ViewModel
+
+class EditProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
