a318c50cdb35f910bdf009bdc8dc8e569680308d
Syntia <53588149+syntialai@users.noreply.github.com>
2020-11-17 18:04:04 +0700

    SETUP-EDIT-PROFILE: Remove unused code

--- features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/editProfile/view/EditProfileActivity.kt
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/editProfile/view/EditProfileActivity.kt
@@ -2,16 +2,12 @@ package com.future.tailormade_profile.feature.editProfile.view
 
 import android.os.Bundle
 import com.future.tailormade.base.view.BaseActivity
-import com.future.tailormade_profile.core.di.component.ProfileComponentHandler
 import com.future.tailormade_profile.databinding.ActivityEditProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class EditProfileActivity : BaseActivity() {
-
-  @Inject
-  lateinit var profileComponentHandler: ProfileComponentHandler
 
   private lateinit var binding: ActivityEditProfileBinding
 
@@ -20,12 +16,5 @@ class EditProfileActivity : BaseActivity() {
     binding = ActivityEditProfileBinding.inflate(layoutInflater)
     toolbar = binding.topToolbarProfile
     setContentView(binding.root)
-
-    profileComponentHandler.create()
-  }
-
-  override fun onDestroy() {
-    profileComponentHandler.destroy()
-    super.onDestroy()
   }
 }
