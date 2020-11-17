f4dc80905d060f118a17925b92629d31419689df
Syntia <53588149+syntialai@users.noreply.github.com>
2020-11-17 20:47:29 +0700

    SETUP-EDIT-ABOUT: Added layout for edit about

---
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/editAbout/viewmodel/EditAboutViewModel.kt
@@ -0,0 +1,11 @@
+package com.future.tailormade_profile.feature.editAbout.viewmodel
+
+import com.future.tailormade.base.viewmodel.BaseViewModel
+
+class EditAboutViewModel : BaseViewModel() {
+
+  override fun getLogName(): String =
+      "com.future.tailormade_profile.feature.editAbout.viewmodel.EditAboutViewModel"
+
+  // TODO: Implement the ViewModel
+}
