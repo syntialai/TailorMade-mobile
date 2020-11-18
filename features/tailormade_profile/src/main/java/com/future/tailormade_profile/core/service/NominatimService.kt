69b6723e0045ee26a1d6979b823d90e1a081891e
Syntia <53588149+syntialai@users.noreply.github.com>
2020-11-18 11:23:53 +0700

    SETUP-EDIT-ABOUT: Added payload and service for edit about

--- features/tailormade_profile/src/main/java/com/future/tailormade_profile/core/service/NominatimService.kt
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/core/service/NominatimService.kt
@@ -3,12 +3,13 @@ package com.future.tailormade_profile.core.service
 import com.future.tailormade_profile.core.api.ProfileApiUrl
 import com.future.tailormade_profile.core.model.response.LocationResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NominatimService {
 
   @GET(ProfileApiUrl.NOMINATIM_API)
-  fun searchLocation(@Query("query") query: String,
+  fun searchLocation(@Path("query") query: String,
       @Query("format") format: String,
       @Query("addressdetails") addressDetails: Int,
       @Query("limit") limit: Int): List<LocationResponse>
--- features/tailormade_profile/src/main/java/com/future/tailormade_profile/core/service/ProfileService.kt
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/core/service/ProfileService.kt
@@ -2,7 +2,9 @@ package com.future.tailormade_profile.core.service
 
 import com.future.tailormade.base.model.response.BaseSingleObjectResponse
 import com.future.tailormade_profile.core.api.ProfileApiUrl
+import com.future.tailormade_profile.core.model.request.UpdateProfileAboutRequest
 import com.future.tailormade_profile.core.model.request.UpdateProfileRequest
+import com.future.tailormade_profile.core.model.response.ProfileAboutResponse
 import com.future.tailormade_profile.core.model.response.ProfileInfoResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -14,7 +16,11 @@ interface ProfileService {
   @GET(ProfileApiUrl.USERS_ID_PATH)
   fun getProfileInfo(@Path("id") id: String): BaseSingleObjectResponse<ProfileInfoResponse>
 
-  @PUT(ProfileApiUrl.USERS_ID_PATH)
+  @PUT(ProfileApiUrl.USERS_ID_UPDATE_BASIC_INFO_PATH)
   fun updateProfileInfo(@Path("id") id: String,
       @Body updateProfileRequest: UpdateProfileRequest): BaseSingleObjectResponse<ProfileInfoResponse>
+
+  @PUT(ProfileApiUrl.USERS_ID_UPDATE_MORE_INFO_PATH)
+  fun updateProfileAboutInfo(@Path("id") id: String,
+      @Body updateProfileAboutRequest: UpdateProfileAboutRequest): BaseSingleObjectResponse<ProfileAboutResponse>
 }
