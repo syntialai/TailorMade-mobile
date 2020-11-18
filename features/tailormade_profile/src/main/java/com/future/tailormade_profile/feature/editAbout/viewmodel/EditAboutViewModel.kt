a201b6bf566cc251a9d75017135ba1438e2858eb
Syntia <53588149+syntialai@users.noreply.github.com>
2020-11-18 11:22:12 +0700

    SETUP-EDIT-ABOUT: Added functionality in UI for edit profile about

--- features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/editAbout/viewmodel/EditAboutViewModel.kt
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/editAbout/viewmodel/EditAboutViewModel.kt
@@ -1,11 +1,22 @@
 package com.future.tailormade_profile.feature.editAbout.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.future.tailormade.base.viewmodel.BaseViewModel
+import com.future.tailormade_profile.core.model.entity.Education
+import com.future.tailormade_profile.core.model.entity.Occupation
+import com.future.tailormade_profile.core.model.response.ProfileAboutResponse
 
 class EditAboutViewModel : BaseViewModel() {
 
   override fun getLogName(): String =
       "com.future.tailormade_profile.feature.editAbout.viewmodel.EditAboutViewModel"
 
-  // TODO: Implement the ViewModel
+  private var _profileAbout = MutableLiveData<ProfileAboutResponse>()
+  val profileAbout: LiveData<ProfileAboutResponse>
+    get() = _profileAbout
+
+  fun updateProfileAbout(occupation: Occupation, education: Education) {
+    // TODO: Implement API call
+  }
 }
