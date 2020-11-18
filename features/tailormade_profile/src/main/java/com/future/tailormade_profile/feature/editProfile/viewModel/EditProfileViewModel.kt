62dd88067c5c91f039886491c8a97b20acbb00d7
Syntia <53588149+syntialai@users.noreply.github.com>
2020-11-18 12:06:27 +0700

    SETUP-EDIT-ABOUT: Implements view model

--- features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/editProfile/viewModel/EditProfileViewModel.kt
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/editProfile/viewModel/EditProfileViewModel.kt
@@ -2,12 +2,9 @@ package com.future.tailormade_profile.feature.editProfile.viewModel
 
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.map
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.*
 import com.future.tailormade.base.viewmodel.BaseViewModel
+import com.future.tailormade.config.Constants
 import com.future.tailormade.util.extension.flowOnIO
 import com.future.tailormade.util.extension.flowOnIOwithLoadingDialog
 import com.future.tailormade.util.extension.onError
@@ -57,12 +54,14 @@ class EditProfileViewModel @ViewModelInject constructor(
   @ExperimentalCoroutinesApi
   @InternalCoroutinesApi
   fun updateBasicInfo(name: String, birthDate: Long, phoneNumber: String?, location: String?) {
+    val request = UpdateProfileRequest(name, birthDate, phoneNumber.orEmpty(),
+        location.orEmpty())
     launchViewModelScope {
       authSharedPrefRepository.userId?.let { id ->
-        profileRepository.updateProfileInfo(id,
-            UpdateProfileRequest(name, birthDate, phoneNumber.orEmpty(),
-                location.orEmpty())).flowOnIOwithLoadingDialog(this).onError {
-          _errorMessage.postValue("Failed to update profile")
+        profileRepository.updateProfileInfo(id, request).flowOnIOwithLoadingDialog(
+            this).onError {
+          _errorMessage.postValue(Constants.FAILED_TO_UPDATE_PROFILE)
+          appLogger.logOnError(Constants.FAILED_TO_UPDATE_PROFILE, it)
         }.collect { response ->
           response.data?.let {
             _profileInfo.postValue(it)
