bab3e42bf89ba055047e63f6bf4f35d635690404
syntialai <53588149+syntialai@users.noreply.github.com>
2020-11-20 00:10:57 +0700

    SETUP-APP-SETTINGS: Fix settings view

--- features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/settings/SettingsActivity.kt
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/settings/SettingsActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.future.tailormade.base.view.BaseActivity
 import com.future.tailormade_profile.R
+import com.future.tailormade_profile.databinding.ActivitySettingsBinding
 
 class SettingsActivity : BaseActivity() {
 
+  private lateinit var binding: ActivitySettingsBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_settings)
+    binding = ActivitySettingsBinding.inflate(layoutInflater)
+    toolbar = binding.topToolbarSettings
+    setContentView(binding.root)
+    setupToolbar(getString(R.string.settings_label))
+
     if (savedInstanceState == null) {
-      supportFragmentManager.beginTransaction().replace(R.id.settings,
+      supportFragmentManager.beginTransaction().replace(binding.flSettings.id,
           SettingsFragment()).commit()
     }
-    supportActionBar?.setDisplayHomeAsUpEnabled(true)
   }
 
   class SettingsFragment : PreferenceFragmentCompat() {
