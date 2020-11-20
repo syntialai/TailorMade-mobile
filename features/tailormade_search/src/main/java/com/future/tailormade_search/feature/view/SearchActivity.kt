15bf6180c17ed9ab8ea3c629560cf976a250facc
Syntia <53588149+syntialai@users.noreply.github.com>
2020-11-20 15:43:39 +0700

    SETUP-SEARCH-ACTIVITY: Setup search activity layout

--- features/tailormade_search/src/main/java/com/future/tailormade_search/feature/view/SearchActivity.kt
+++ features/tailormade_search/src/main/java/com/future/tailormade_search/feature/view/SearchActivity.kt
@@ -1,13 +1,29 @@
 package com.future.tailormade_search.feature.view
 
 import android.os.Bundle
+import android.view.View
 import com.future.tailormade.base.view.BaseActivity
-import com.future.tailormade_search.R
+import com.future.tailormade_search.databinding.ActivitySearchBinding
 
 class SearchActivity : BaseActivity() {
 
+  private lateinit var binding: ActivitySearchBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_search)
+    binding = ActivitySearchBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+  }
+
+  fun hideInitialSearchState() {
+    with(binding) {
+      imageViewSearchState.visibility = View.GONE
+      textViewSearchState.visibility = View.GONE
+      textViewSearchDescriptionState.visibility = View.GONE
+    }
+  }
+
+  fun showFragment() {
+
   }
 }
