8e3c5480b172e69914aa3dcdd486c82bba3f4968
Syntia <53588149+syntialai@users.noreply.github.com>
2020-11-20 09:47:10 +0700

    SETUP-SEARCH-ACTIVITY: Setup module

---
+++ features/tailormade_search/src/main/java/com/future/tailormade_search/feature/view/SearchActivity.kt
@@ -0,0 +1,13 @@
+package com.future.tailormade_search.feature.view
+
+import android.os.Bundle
+import com.future.tailormade.base.view.BaseActivity
+import com.future.tailormade_search.R
+
+class SearchActivity : BaseActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_search)
+  }
+}
