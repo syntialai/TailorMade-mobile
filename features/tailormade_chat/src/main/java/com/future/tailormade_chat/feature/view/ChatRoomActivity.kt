f74fa2c5438324996f3b8b3da44472dc47d3ca94
syntialai <53588149+syntialai@users.noreply.github.com>
2020-11-30 00:00:34 +0700

    SETUP-CHAT-ROOM: Add adapter and view model for chat room, fix layout for chat room item, and provide functionality in activity
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- features/tailormade_chat/src/main/java/com/future/tailormade_chat/feature/view/ChatRoomActivity.kt
+++ features/tailormade_chat/src/main/java/com/future/tailormade_chat/feature/view/ChatRoomActivity.kt
@@ -1,21 +1,72 @@
 package com.future.tailormade_chat.feature.view
 
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import com.future.tailormade.base.view.BaseActivity
-import com.future.tailormade_chat.R
+import com.future.tailormade_auth.core.repository.impl.AuthSharedPrefRepository
+import com.future.tailormade_chat.core.model.entity.ChatRoom
 import com.future.tailormade_chat.databinding.ActivityChatRoomBinding
+import com.future.tailormade_chat.feature.adapter.ChatRoomAdapter
+import com.future.tailormade_chat.feature.viewModel.ChatRoomViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ChatRoomActivity : BaseActivity() {
 
+  @Inject
+  lateinit var authSharedPrefRepository: AuthSharedPrefRepository
+
   private lateinit var binding: ActivityChatRoomBinding
+
+  private val viewModel: ChatRoomViewModel by viewModels()
+
+  private val adapterValueEventListener by lazy {
+    object : ValueEventListener {
+
+      @RequiresApi(Build.VERSION_CODES.N)
+      override fun onDataChange(snapshot: DataSnapshot) {
+        val chatRoom = snapshot.value as ChatRoom
+        adapter.submitList(chatRoom.chats.values.toList())
+      }
+
+      override fun onCancelled(error: DatabaseError) {
+        // TODO: Provide error
+      }
+    }
+  }
+
+  private var adapter = ChatRoomAdapter(authSharedPrefRepository.userId.orEmpty())
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityChatRoomBinding.inflate(layoutInflater)
     toolbar = binding.topToolbarChatRoom
     setContentView(binding.root)
+
+    getIntentData()
+    setupObserver()
+  }
+
+  private fun getIntentData() {
+    intent.getStringExtra(PARAM_CHAT_ROOM_ID)?.let { chatRoomId ->
+      viewModel.setChatRoomId(chatRoomId)
+    }
+  }
+
+  private fun setupObserver() {
+    viewModel.chatRoomId.observe(this, {
+      viewModel.fetchChatRoomData()
+    })
+
+    viewModel.chatRoomContent.observe(this, {
+      it.addValueEventListener(adapterValueEventListener)
+    })
   }
 
   companion object {
