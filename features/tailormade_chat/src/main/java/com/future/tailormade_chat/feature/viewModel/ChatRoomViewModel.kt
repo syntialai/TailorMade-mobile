b9101bf005786059fbcaf0bba62e58eaff5b7367
syntialai <53588149+syntialai@users.noreply.github.com>
2020-11-30 01:20:52 +0700

    SETUP-CHAT-ROOM: Add send messages functionality
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- features/tailormade_chat/src/main/java/com/future/tailormade_chat/feature/viewModel/ChatRoomViewModel.kt
+++ features/tailormade_chat/src/main/java/com/future/tailormade_chat/feature/viewModel/ChatRoomViewModel.kt
@@ -1,13 +1,20 @@
 package com.future.tailormade_chat.feature.viewModel
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.future.tailormade.base.viewmodel.BaseViewModel
+import com.future.tailormade.config.Constants
 import com.future.tailormade_auth.core.repository.impl.AuthSharedPrefRepository
-import com.future.tailormade_chat.core.model.entity.ChatRoom
+import com.future.tailormade_chat.core.model.entity.Chat
+import com.future.tailormade_chat.core.model.entity.Text
 import com.future.tailormade_chat.core.repository.RealtimeDbRepository
 import com.google.firebase.database.Query
+import java.sql.Timestamp
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 class ChatRoomViewModel @ViewModelInject constructor(
     private val realtimeDbRepository: RealtimeDbRepository,
@@ -33,5 +40,16 @@ class ChatRoomViewModel @ViewModelInject constructor(
 
   fun setChatRoomId(chatRoomId: String) {
     _chatRoomId.value = chatRoomId
+  }
+
+  @RequiresApi(Build.VERSION_CODES.O)
+  fun sendMessage(text: String) {
+    authSharedPrefRepository.userId?.let { userId ->
+      val chat = Chat(
+          Timestamp(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)), userId,
+          false, Constants.MESSAGES_TYPE_TEXT, Text(text))
+      realtimeDbRepository.updateChatRoom("", chat)
+      // TODO: Add on successs and failure listener
+    }
   }
 }
--- libraries/tailormade_base/src/main/java/com/future/tailormade/config/Constants.kt
+++ libraries/tailormade_base/src/main/java/com/future/tailormade/config/Constants.kt
@@ -32,6 +32,8 @@ object Constants {
     const val FAILED_TO_GET_PROFILE_INFO = "$FAILED_TO get profile info"
     const val FAILED_TO_UPDATE_PROFILE = "$FAILED_TO update profile"
 
+    const val MESSAGES_TYPE_TEXT = "TEXT"
+
     /*
      * Date Patterns
      */
