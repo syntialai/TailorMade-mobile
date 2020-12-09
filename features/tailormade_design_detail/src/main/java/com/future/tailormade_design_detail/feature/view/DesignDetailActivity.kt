fa5abb0fd57f019a817fea2f498c98431ceaa9ed
Syntia <53588149+syntialai@users.noreply.github.com>
2020-12-09 18:26:38 +0700

    SETUP-DESIGN-DETAIL-LAYOUT: Add activity and router for design detail
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

---
+++ features/tailormade_design_detail/src/main/java/com/future/tailormade_design_detail/feature/view/DesignDetailActivity.kt
@@ -0,0 +1,33 @@
+package com.future.tailormade_design_detail.feature.view
+
+import android.os.Bundle
+import com.future.tailormade.base.view.BaseActivity
+import com.future.tailormade_design_detail.databinding.ActivityDesignDetailBinding
+
+class DesignDetailActivity : BaseActivity() {
+
+  companion object {
+    private const val PARAM_DESIGN_DETAIL_ID = "DESIGN_DETAIL_ID"
+  }
+
+  private lateinit var binding: ActivityDesignDetailBinding
+
+  private var _designDetailId: String = ""
+
+  override fun getScreenName(): String = "Design Detail Page"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityDesignDetailBinding.inflate(layoutInflater)
+    toolbar = binding.topToolbarDesignDetail
+    setContentView(binding.root)
+
+    setDesignDetailId(intent?.getStringExtra(PARAM_DESIGN_DETAIL_ID).orEmpty())
+  }
+
+  fun getId() = _designDetailId
+
+  private fun setDesignDetailId(id: String) {
+    _designDetailId = id
+  }
+}
--- libraries/tailormade_router/src/main/java/com/future/tailormade_router/actions/Action.kt
+++ libraries/tailormade_router/src/main/java/com/future/tailormade_router/actions/Action.kt
@@ -5,22 +5,32 @@ import android.content.Intent
 
 object Action {
 
-    /*
-     * Action name
-     */
-    private const val ACTION_OPEN_SIGN_IN = "com.future.tailormade.signIn.open"
-    private const val ACTION_OPEN_PROFILE = "com.future.tailormade.editProfile.open"
-    private const val ACTION_OPEN_SETTINGS = "com.future.tailormade.settings.open"
+  /**
+   * Param name
+   */
+  private const val PARAM_DESIGN_DETAIL_ID = "DESIGN_DETAIL_ID"
 
-    /*
-     * Action function
-     */
-    fun goToSignIn(context: Context) = getIntent(context, ACTION_OPEN_SIGN_IN)
+  /**
+   * Action name
+   */
+  private const val ACTION_OPEN_SIGN_IN = "com.future.tailormade.signIn.open"
+  private const val ACTION_OPEN_PROFILE = "com.future.tailormade.editProfile.open"
+  private const val ACTION_OPEN_SETTINGS = "com.future.tailormade.settings.open"
+  private const val ACTION_OPEN_DESIGN_DETAIL = "com.future.tailormade.designDetail.open"
 
-    fun goToProfile(context: Context) = getIntent(context, ACTION_OPEN_PROFILE)
+  /**
+   * Action function
+   */
+  fun goToSignIn(context: Context) = getIntent(context, ACTION_OPEN_SIGN_IN)
 
-    fun goToSettings(context: Context) = getIntent(context, ACTION_OPEN_SETTINGS)
+  fun goToProfile(context: Context) = getIntent(context, ACTION_OPEN_PROFILE)
 
-    private fun getIntent(context: Context, action: String) =
-        Intent(action).setPackage(context.packageName)
+  fun goToSettings(context: Context) = getIntent(context, ACTION_OPEN_SETTINGS)
+
+  fun goToDesignDetail(context: Context, id: String) = getIntent(context, ACTION_OPEN_DESIGN_DETAIL).apply {
+    putExtra(PARAM_DESIGN_DETAIL_ID, id)
+  }
+
+  private fun getIntent(context: Context, action: String) = Intent(action).setPackage(
+      context.packageName)
 }
