65d2a62d1fe10098557eae53cb5d46aa02bbcf43
Syntia <53588149+syntialai@users.noreply.github.com>
2020-12-10 01:11:40 +0700

    DESIGN-DETAIL-CORE: Added api url, di, model and service
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

---
+++ features/tailormade_design_detail/src/main/java/com/future/tailormade_design_detail/core/model/response/DesignDetailResponse.kt
@@ -0,0 +1,28 @@
+package com.future.tailormade_design_detail.core.model.response
+
+data class DesignDetailResponse(
+
+    val id: String,
+
+    val title: String,
+
+    val image: String,
+
+    val price: Double,
+
+    val discount: Double = 0.0,
+
+    val tailorId: String,
+
+    val tailorName: String,
+
+    val description: String,
+
+    val size: List<SizeResponse>,
+
+    val color: List<ColorResponse>,
+
+    val category: List<String>,
+
+    val active: Boolean
+)
