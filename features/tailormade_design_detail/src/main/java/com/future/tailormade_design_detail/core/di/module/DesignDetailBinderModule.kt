803f2ef746f92ed742ee7e0c2c6eb2f11353b8d8
Syntia <53588149+syntialai@users.noreply.github.com>
2020-12-12 17:05:50 +0700

    DESIGN-DETAIL-CORE: Change component to install repository binder
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- features/tailormade_design_detail/src/main/java/com/future/tailormade_design_detail/core/di/module/DesignDetailBinderModule.kt
+++ features/tailormade_design_detail/src/main/java/com/future/tailormade_design_detail/core/di/module/DesignDetailBinderModule.kt
@@ -5,10 +5,10 @@ import com.future.tailormade_design_detail.core.repository.impl.DesignDetailRepo
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.components.ActivityRetainedComponent
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(ActivityRetainedComponent::class)
 abstract class DesignDetailBinderModule {
 
   @Binds
