11264d8714235f6e1030f35bd371cdf44ee83fef
Syntia <53588149+syntialai@users.noreply.github.com>
2020-12-16 00:18:43 +0700

    CHECKOUT: Add functionality for bottom sheet
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- app/src/main/java/com/future/tailormade/feature/checkout/view/CheckoutEditMeasurementBottomSheetDialogFragment.kt
+++ app/src/main/java/com/future/tailormade/feature/checkout/view/CheckoutEditMeasurementBottomSheetDialogFragment.kt
@@ -4,18 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.future.tailormade.R
 import com.future.tailormade.base.view.BaseBottomSheetDialogFragment
+import com.future.tailormade.core.model.ui.checkout.EditMeasurementFieldUiModel
 import com.future.tailormade.databinding.FragmentCheckoutEditMeasurementBottomSheetDialogBinding
+import com.future.tailormade.feature.checkout.adapter.EditMeasurementFieldAdapter
 
 class CheckoutEditMeasurementBottomSheetDialogFragment : BaseBottomSheetDialogFragment() {
 
 	companion object {
-		fun newInstance() =
-				CheckoutEditMeasurementBottomSheetDialogFragment()
+		fun newInstance(values: MutableList<String>, onSubmitListener: (List<String>) -> Unit) =
+				CheckoutEditMeasurementBottomSheetDialogFragment().apply {
+					this.onSubmitListener = onSubmitListener
+					this.initAdapter()
+					this.insertAdapterValue(values)
+				}
 	}
 
 	private lateinit var binding: FragmentCheckoutEditMeasurementBottomSheetDialogBinding
+	private lateinit var onSubmitListener: (List<String>) -> Unit
+
+	private val editMeasurementAdapter by lazy {
+		EditMeasurementFieldAdapter()
+	}
+	private val editMeasurementList by lazy {
+		mutableListOf<EditMeasurementFieldUiModel>()
+	}
 
 	override fun getScreenName(): String = "Edit Measurement"
 
@@ -23,6 +38,41 @@ class CheckoutEditMeasurementBottomSheetDialogFragment : BaseBottomSheetDialogFr
 			savedInstanceState: Bundle?): View {
 		binding = FragmentCheckoutEditMeasurementBottomSheetDialogBinding.inflate(inflater, container,
 				false)
+		with(binding) {
+			buttonCloseEditMeasurementDialog.setOnClickListener {
+				dismiss()
+			}
+
+			buttonSubmitEditMeasurement.setOnClickListener {
+				onSubmitListener.invoke(editMeasurementList.map { it.value })
+				dismiss()
+			}
+		}
+		initAdapter()
+		setupRecyclerView()
 		return binding.root
+	}
+
+	private fun initAdapter() {
+		val labels = resources.getStringArray(R.array.edit_measurement_type_list)
+		val images = arrayListOf<Int>() // TODO: Add static resource
+		labels.forEachIndexed { index, label ->
+			val editMeasurementUiModel = EditMeasurementFieldUiModel(images[index], label)
+			editMeasurementList.add(editMeasurementUiModel)
+		}
+		editMeasurementAdapter.submitList(editMeasurementList)
+	}
+
+	private fun insertAdapterValue(values: MutableList<String>) {
+		editMeasurementList.forEachIndexed { index, editMeasurement ->
+			editMeasurement.value = values[index]
+		}
+	}
+
+	private fun setupRecyclerView() {
+		with(binding.recyclerViewEditMeasurement) {
+			layoutManager = LinearLayoutManager(context)
+			adapter = editMeasurementAdapter
+		}
 	}
 }
