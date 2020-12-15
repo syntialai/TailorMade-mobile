def146bcd223d6728a3e6349066ed31c048a0d4a
Syntia <53588149+syntialai@users.noreply.github.com>
2020-12-16 01:41:22 +0700

    CHECKOUT: Add repository and bind to module
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

---
+++ app/src/main/java/com/future/tailormade/core/repository/CheckoutRepository.kt
@@ -0,0 +1,12 @@
+package com.future.tailormade.core.repository
+
+import com.future.tailormade.base.model.response.BaseSingleObjectResponse
+import com.future.tailormade.core.model.request.checkout.CheckoutRequest
+import com.future.tailormade.core.model.response.checkout.CheckoutResponse
+import kotlinx.coroutines.flow.Flow
+
+interface CheckoutRepository {
+
+	suspend fun checkoutCartItem(id: String, checkoutRequest: CheckoutRequest):
+			Flow<BaseSingleObjectResponse<CheckoutResponse>>
+}
---
+++ app/src/main/java/com/future/tailormade/core/repository/impl/CheckoutRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.future.tailormade.core.repository.impl
+
+import com.future.tailormade.base.repository.BaseRepository
+import com.future.tailormade.core.model.request.checkout.CheckoutRequest
+import com.future.tailormade.core.repository.CheckoutRepository
+import com.future.tailormade.core.service.CheckoutService
+import com.future.tailormade.util.extension.flowOnIO
+import javax.inject.Inject
+import kotlinx.coroutines.flow.flow
+
+class CheckoutRepositoryImpl @Inject constructor(private val checkoutService: CheckoutService) :
+		BaseRepository(), CheckoutRepository {
+
+	override fun getLogName() = "com.future.tailormade.core.repository.impl.CheckoutRepositoryImpl"
+
+	override suspend fun checkoutCartItem(id: String, checkoutRequest: CheckoutRequest) = flow {
+		emit(checkoutService.postCheckoutCartItem(id, checkoutRequest))
+	}.flowOnIO()
+}
