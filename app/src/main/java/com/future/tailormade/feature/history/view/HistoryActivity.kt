c422caac7b8529bcdea77b8ad332a6b36af5f203
Syntia <53588149+syntialai@users.noreply.github.com>
2020-12-17 23:40:05 +0700

    HISTORY: Add history activity and history item layout
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

---
+++ app/src/main/java/com/future/tailormade/feature/history/view/HistoryActivity.kt
@@ -0,0 +1,18 @@
+package com.future.tailormade.feature.history.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.future.tailormade.base.view.BaseActivity
+import com.future.tailormade.databinding.ActivityHistoryBinding
+
+class HistoryActivity : BaseActivity() {
+
+	private lateinit var binding: ActivityHistoryBinding
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityHistoryBinding.inflate(layoutInflater)
+		toolbar = binding.topToolbarHistory
+		setContentView(binding.root)
+	}
+}
