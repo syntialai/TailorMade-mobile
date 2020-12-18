afe9d03874364284de4a44852a1b607f184c799b
Syntia <53588149+syntialai@users.noreply.github.com>
2020-12-19 01:45:05 +0700

    HISTORY: Add functionality for history detail ui
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- app/src/main/java/com/future/tailormade/feature/history/view/HistoryDetailFragment.kt
+++ app/src/main/java/com/future/tailormade/feature/history/view/HistoryDetailFragment.kt
@@ -9,6 +9,7 @@ import com.future.tailormade.base.view.BaseFragment
 import com.future.tailormade.base.viewmodel.BaseViewModel
 import com.future.tailormade.databinding.FragmentHistoryDetailBinding
 import com.future.tailormade.feature.history.viewModel.HistoryDetailViewModel
+import com.future.tailormade.util.extension.show
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,5 +31,46 @@ class HistoryDetailFragment : BaseFragment() {
 			savedInstanceState: Bundle?): View {
 		binding = FragmentHistoryDetailBinding.inflate(inflater, container, false)
 		return binding.root
+	}
+
+	override fun setupFragmentObserver() {
+		super.setupFragmentObserver()
+
+		// TODO: Observe data
+	}
+
+	private fun setupDesignDetailData() {
+		with(binding) {
+			// TODO: set this method when layout is exist
+		}
+	}
+
+	private fun setupMeasurementDetailData() {
+		with(binding) {
+			// TODO: set this method when layout is exist
+		}
+	}
+
+	private fun setupOrderInfoData(orderId: String, orderedBy: String, orderDate: String) {
+		with(binding.layoutCardHistoryDetailOrderInfo) {
+			textViewHistoryDetailOrderId.text = orderId
+			textViewHistoryDetailOrderBy.text = orderedBy
+			textViewHistoryDetailOrderDate.text = orderDate
+		}
+	}
+
+	private fun setupPaymentData(quantity: String, priceTotal: String, discountTotal: String,
+			paymentTotal: String) {
+		with(binding) {
+			// TODO: set this method when layout is exist
+		}
+	}
+
+	private fun showSpecialInstruction(instruction: String) {
+		with(binding) {
+			textViewHistoryDetailSpecialInstructionsTitle.show()
+			textViewHistoryDetailSpecialInstructions.show()
+			textViewHistoryDetailSpecialInstructions.text = instruction
+		}
 	}
 }
