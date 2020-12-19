9e285c18009b38c17131d77ac0858092643fa2fa
Syntia <53588149+syntialai@users.noreply.github.com>
2020-12-19 16:22:38 +0700

    PROFILE_DESIGN_LIST: Revise and add mapper to ProfileViewModel
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

---
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/core/model/ui/ProfileInfoUiModel.kt
@@ -0,0 +1,25 @@
+package com.future.tailormade_profile.core.model.ui
+
+import com.future.tailormade_profile.core.model.entity.Education
+import com.future.tailormade_profile.core.model.entity.Location
+import com.future.tailormade_profile.core.model.entity.Occupation
+
+data class ProfileInfoUiModel(
+
+		var id: String,
+
+		var name: String,
+
+		var image: String? = null,
+
+		var phoneNumber: String? = null,
+
+		var birthDate: Long = 0L,
+		
+		var address: String,
+
+		var location: Location? = null,
+
+		var occupation: Occupation? = null,
+
+		var education: Education? = null)
