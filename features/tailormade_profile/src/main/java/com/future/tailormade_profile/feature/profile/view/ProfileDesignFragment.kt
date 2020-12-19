d9045defa4e0a415c117790ec34e92ae92943cec
Syntia <53588149+syntialai@users.noreply.github.com>
2020-12-19 13:50:13 +0700

    PROFILE_DESIGN_LIST: Add layout and setup ui functionality for profile design
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

---
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/profile/view/ProfileDesignFragment.kt
@@ -0,0 +1,60 @@
+package com.future.tailormade_profile.feature.profile.view
+
+import android.R.attr.spacing
+import android.graphics.Rect
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.setPadding
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import com.future.tailormade.base.view.BaseFragment
+import com.future.tailormade.base.viewmodel.BaseViewModel
+import com.future.tailormade_profile.R
+import com.future.tailormade_profile.databinding.FragmentProfileDesignBinding
+import com.future.tailormade_profile.feature.profile.viewModel.ProfileDesignViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileDesignFragment : BaseFragment() {
+
+	companion object {
+		fun newInstance() = ProfileDesignFragment()
+	}
+
+	private lateinit var binding: FragmentProfileDesignBinding
+
+	private val viewModel: ProfileDesignViewModel by viewModels()
+
+	override fun getLogName() =
+			"com.future.tailormade_profile.feature.profile.view.ProfileDesignFragment"
+
+	override fun getViewModel(): BaseViewModel = viewModel
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+			savedInstanceState: Bundle?): View {
+		binding = FragmentProfileDesignBinding.inflate(inflater, container, false)
+		setupRecyclerView()
+		return binding.root
+	}
+
+	private fun setupRecyclerView() {
+		with(binding.recyclerViewProfileDesign) {
+			layoutManager = GridLayoutManager(context, 3)
+//			adapter =
+			setPadding(resources.getDimensionPixelSize(R.dimen.dp_4))
+			clipToPadding = false
+			clipChildren = false
+			addItemDecoration(object : ItemDecoration() {
+
+				override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView,
+						state: RecyclerView.State) {
+					outRect.set(spacing, spacing, spacing, spacing)
+				}
+			})
+		}
+	}
+}
---
+++ features/tailormade_profile/src/main/java/com/future/tailormade_profile/feature/profile/viewModel/ProfileDesignViewModel.kt
@@ -0,0 +1,11 @@
+package com.future.tailormade_profile.feature.profile.viewModel
+
+import com.future.tailormade.base.viewmodel.BaseViewModel
+
+class ProfileDesignViewModel : BaseViewModel() {
+
+	override fun getLogName() =
+	    "com.future.tailormade_profile.feature.profile.viewModel.ProfileDesignViewModel"
+
+	// TODO: Implement the ViewModel
+}
