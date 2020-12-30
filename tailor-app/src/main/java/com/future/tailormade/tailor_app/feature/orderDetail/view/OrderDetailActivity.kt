0fdc63533c3f8ce87e657c3333ac23db7e8a2b24
syntialai <53588149+syntialai@users.noreply.github.com>
2020-12-30 11:28:02 +0700

    TAILOR_ORDERS: Added order detail layout
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

---
+++ tailor-app/src/main/java/com/future/tailormade/tailor_app/feature/orderDetail/view/OrderDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.future.tailormade.tailor_app.feature.orderDetail.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.future.tailormade.base.view.BaseActivity
+import com.future.tailormade.tailor_app.R
+
+class OrderDetailActivity : BaseActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_order_detail)
+  }
+}
