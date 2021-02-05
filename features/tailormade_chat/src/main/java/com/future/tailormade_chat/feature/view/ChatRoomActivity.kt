8dee383a42f5a97fdc0aa0a991d29ac14d3257ef
syntialai <53588149+syntialai@users.noreply.github.com>
2021-02-05 18:07:10 +0700

    LOADING_STATE: Fix bottom nav view
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- features/tailormade_chat/src/main/java/com/future/tailormade_chat/feature/view/ChatRoomActivity.kt
+++ features/tailormade_chat/src/main/java/com/future/tailormade_chat/feature/view/ChatRoomActivity.kt
@@ -45,8 +45,7 @@ class ChatRoomActivity : BaseActivity() {
         val chatRoom = snapshot.getValue(ChatRoom::class.java)
         chatRoom?.chats?.let {
           chatRoomAdapter.submitList(it.toSortedMap().values.toList())
-          chatRoomAdapter.notifyDataSetChanged()
-          binding.recyclerViewChatRoom.scrollToPosition(it.size - 1)
+          scrollRecyclerView(it.size - 1)
         }
       }
 
@@ -90,6 +89,10 @@ class ChatRoomActivity : BaseActivity() {
   private fun removeMessage() {
     binding.layoutInputTextChatRoom.editTextMessageChatRoom.text = null
     viewModel.setIsSent(null)
+  }
+
+  private fun scrollRecyclerView(position: Int) {
+    binding.recyclerViewChatRoom.scrollToPosition(position)
   }
 
   @RequiresApi(Build.VERSION_CODES.O)
