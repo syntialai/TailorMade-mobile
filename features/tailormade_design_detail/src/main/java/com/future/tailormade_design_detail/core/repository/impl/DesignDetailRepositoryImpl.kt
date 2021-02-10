e7747a451c73418a5efde89a6c5e0fed6b772098
syntialai <53588149+syntialai@users.noreply.github.com>
2021-02-10 15:12:30 +0700

    DESIGN_DETAIL_UNIT_TEST: Add design detail repo impl test
    Signed-off-by: syntialai <53588149+syntialai@users.noreply.github.com>

--- features/tailormade_design_detail/src/main/java/com/future/tailormade_design_detail/core/repository/impl/DesignDetailRepositoryImpl.kt
+++ features/tailormade_design_detail/src/main/java/com/future/tailormade_design_detail/core/repository/impl/DesignDetailRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.future.tailormade_design_detail.core.repository.impl
 import com.future.tailormade.base.model.BaseMapperModel
 import com.future.tailormade.base.repository.BaseRepository
 import com.future.tailormade.config.Constants
-import com.future.tailormade.util.extension.flowOnIO
 import com.future.tailormade_design_detail.core.mapper.DesignDetailMapper
 import com.future.tailormade_design_detail.core.model.request.cart.AddToCartRequest
 import com.future.tailormade_design_detail.core.model.request.design.DesignRequest
@@ -13,44 +12,46 @@ import com.future.tailormade_design_detail.core.repository.DesignDetailRepositor
 import com.future.tailormade_design_detail.core.service.DesignDetailService
 import java.io.File
 import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 
 class DesignDetailRepositoryImpl @Inject constructor(
-    private val designDetailService: DesignDetailService) : BaseRepository(),
-    DesignDetailRepository {
+    private val designDetailService: DesignDetailService,
+    private val ioDispatcher: CoroutineDispatcher) : BaseRepository(), DesignDetailRepository {
 
   companion object {
-    private const val FILE = "file"
+    const val FILE = "file"
   }
 
-  override fun getLogName() = "com.future.tailormade_design_detail.core.repository.impl.DesignDetailRepositoryImpl"
+  override fun getLogName() =
+      "com.future.tailormade_design_detail.core.repository.impl.DesignDetailRepositoryImpl"
 
   override suspend fun getDesignDetailById(id: String) = flow {
     val designDetailResponse = designDetailService.getDesignDetailById(id).data
-//    val designDetailResponse = DataMock.getDesignDetailMock()
     designDetailResponse?.let { data ->
       val designDetailUiModel = DesignDetailMapper.mapToDesignDetailUiModel(data)
       emit(BaseMapperModel(data, designDetailUiModel))
     }
-  }.flowOnIO()
+  }.flowOn(ioDispatcher)
 
   override suspend fun addDesignByTailor(
       tailorId: String, designRequest: DesignRequest): Flow<DesignDetailResponse> = flow {
     designDetailService.postAddDesignByTailor(tailorId, designRequest).data?.let {
       emit(it)
     }
-  }.flowOnIO()
+  }.flowOn(ioDispatcher)
 
   override suspend fun addToCart(
       userId: String, addToCartRequest: AddToCartRequest): Flow<AddToCartResponse> = flow {
     designDetailService.postAddToCart(userId, addToCartRequest).data?.let {
       emit(it)
     }
-  }.flowOnIO()
+  }.flowOn(ioDispatcher)
 
   override suspend fun updateDesignById(
       tailorId: String, id: String, designRequest: DesignRequest) = flow {
@@ -59,7 +60,7 @@ class DesignDetailRepositoryImpl @Inject constructor(
       val designDetailUiModel = DesignDetailMapper.mapToDesignDetailUiModel(data)
       emit(BaseMapperModel(data, designDetailUiModel))
     }
-  }.flowOnIO()
+  }.flowOn(ioDispatcher)
 
   override suspend fun uploadDesignImage(file: File) = flow {
     if (file.exists().not()) {
@@ -71,5 +72,5 @@ class DesignDetailRepositoryImpl @Inject constructor(
         RequestBody.create(MediaType.parse(Constants.TYPE_IMAGE_JPEG), file))
     emit(designDetailService.postUploadImage(
         Constants.UPLOAD_TYPE_DESIGN, imageMultiPartBody).data?.imageUrl.orEmpty())
-  }.flowOnIO()
+  }.flowOn(ioDispatcher)
 }
